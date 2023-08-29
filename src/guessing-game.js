1459efc1c9c87e6f41e828fc88e04f5827077528
dimasiberia <dima.siberia@icloud.com>
2023-08-29 19:01:45 +0700

    init: start task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        let mid = (this.min+((this.max-this.min) / 2))
 
+        return Math.round(mid)
     }
 
     lower() {
+        this.max = Math.round((this.min+((this.max-this.min) / 2)))
+
 
     }
 
     greater() {
+        this.min = Math.round((this.min+((this.max-this.min) / 2)))
 
     }
 }
